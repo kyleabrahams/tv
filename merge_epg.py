c05ea2359709fbddde1a56a373b21be91027702b
kyleabrahams <47188480+kyleabrahams@users.noreply.github.com>
2024-11-05 22:20:47 -0500

    1020

--- merge_epg.py
+++ merge_epg.py
@@ -1,7 +1,7 @@
 import requests
 import xml.etree.ElementTree as ET
 import logging
-from bs4 import BeautifulSoup  # Import BeautifulSoup for HTML parsing
+import os
 
 # List of EPG sources to merge run it 
 # python3 merge_epg.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup  # Import BeautifulSoup for HTML parsing
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 # List of EPG source URLs to merge
 epg_urls = [
@@ -42,23 +43,32 @@ def fetch_epg_data(url):
     response = requests.get(url)
     if response.status_code == 200:
         if response.content.strip():
-            epg_tree = ET.ElementTree(ET.fromstring(response.content))
+            try:
+                epg_tree = ET.ElementTree(ET.fromstring(response.content))
+                return epg_tree
+            except ET.ParseError as e:
+                logging.error(f"XML parse error for {url}: {e}")
+                return None
         else:
-            print(f"No content fetched from {url}")
+            logging.warning(f"No content fetched from {url}")
             return None
     else:
-        print(f"Error fetching {url}: {response.status_code}")
+        logging.error(f"Error fetching {url}: {response.status_code}")
         return None
 
 # Create root element for the merged EPG
 merged_root = ET.Element("tv")
 
 # Fetch and merge EPG data
+seen_channels = set()
 for url in epg_urls:
+    logging.info(f"Fetching EPG data from {url}")
     epg_tree = fetch_epg_data(url)
     if epg_tree:
         for element in epg_tree.getroot():
-            merged_root.append(element)
+            if element.tag == "channel" and element.attrib['id'] not in seen_channels:
+                merged_root.append(element)
+                seen_channels.add(element.attrib['id'])
 
 # Save merged EPG to file
 try:
