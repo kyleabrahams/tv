61b786810e091f5e8c7ec4dbee53d8932eb0459c
kyleabrahams <47188480+kyleabrahams@users.noreply.github.com>
2024-12-20 11:38:28 -0500

    1138a Dec 20

--- scripts/playlist_generator.py
+++ scripts/playlist_generator.py
@@ -16,16 +16,18 @@ def generate_m3u(start, end, base_url, output_dir, output_file_name):
         f.write("#EXTM3U\n\n")
         for channel in range(start, end + 1):
             f.write(f"#EXTINF:-1 ,channel {channel}\n")
-            # f.write(f"{base_url}/{channel}.m3u8\n")
-            f.write(f"{base_url}/{channel}\n")  # Removed .m3u8
+            # f.write(f"{base_url}/{channel}.m3u8\n") # With .m3u8
+            f.write(f"{base_url}/{channel}\n")
+
     print(f"M3U playlist generated: {output_file}")
 
 # Parameters
-start_channel = 418000  # Starting channel number
-end_channel = 425000  # Ending channel number
-base_url = "http://23.227.140.75:3050/mcneelyken@ymail.com/9706320021"  # Base URL for the streams
+start_channel = 46000  # Starting channel number
+end_channel = 60000  # Ending channel number
+# base_url = "http://xxx/xxx/xxx"  # Base URL for the streams
+base_url = "http://fortv.cc:8080/105094817/105094817"  # Base URL for the streams
 output_dir = "/Volumes/Kyle4tb1223/_Android/_M3U/___ServersFULL"  # Directory to save the file
-output_file_name = "23-227-140-75-3050FULL.m3u"  # Output file name
+output_file_name = "fortv-ccDec20.m3u"  # Output file name
 
 # Generate the M3U playlist
 generate_m3u(start_channel, end_channel, base_url, output_dir, output_file_name)
