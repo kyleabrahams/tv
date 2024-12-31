e9356d0efad44b8ed4b547a7258a83cd288bc968
kyleabrahams <47188480+kyleabrahams@users.noreply.github.com>
2024-12-31 12:38:38 -0500

    Dec 31 1238p

--- scripts/playlist_generator.py
+++ scripts/playlist_generator.py
@@ -15,23 +15,23 @@ def generate_m3u(start, end, base_url, output_dir, output_file_name):
         f.write("#EXTM3U\n\n")
         for channel in range(start, end + 1):
             f.write(f"#EXTINF:-1 ,channel {channel}\n")
-            f.write(f"{base_url}/{channel}.m3u8\n") # With .m3u8
-            # f.write(f"{base_url}/{channel}\n")
+            # f.write(f"{base_url}/{channel}.m3u8\n") # With .m3u8
+            f.write(f"{base_url}/{channel}\n")
 
     print(f"M3U playlist generated: {output_file}")
 
 # Parameters
-start_channel = 1  # Starting channel number
-end_channel = 15000  # Ending channel number
+start_channel = 46000  # Starting channel number
+end_channel = 56999  # Ending channel number
 # base_url = "http://server/usepassword"  # Base URL for the streams
-base_url = "http://cdn1host.online:2999/live/venomj/EmoOmOskzq"  # Base URL for the streams
+base_url = "http://fortv.cc:8080/105094817/105094817"  # Base URL for the streams
 
 # Set the output directory relative to "/Volumes/Kyle4tb1223"
 output_dir = os.path.join(os.path.expanduser('~'), "Documents/___ServersFULL")
 
 # Include today's date in the output file name
 today_date = datetime.now().strftime("%Y%m%d")  # Format: YYYYMMDD
-output_file_name = f"cdn1host-{today_date}.m3u"  # Output file name with today's date
+output_file_name = f"fortv.cc46-57-{today_date}.m3u"  # Output file name with today's date
 
 # python3 playlist_generator.py
 
