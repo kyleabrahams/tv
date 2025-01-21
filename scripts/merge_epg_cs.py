f6791da0cc861fc54fdb584c7dc728fe7475d322
kyleabrahams <47188480+kyleabrahams@users.noreply.github.com>
2025-01-21 20:17:44 +0000

    317

--- scripts/merge_epg_cs.py
+++ scripts/merge_epg_cs.py
@@ -142,10 +142,10 @@ if __name__ == "__main__":
 
 
 # Set up logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+# logger = logging.getLogger(__name__)
+# logging.basicConfig(
+#     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+# )
 
 
 def run_npm_grab():
