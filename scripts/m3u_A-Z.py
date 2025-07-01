e911b850402ea1c1ec5830918debadb063563acf
kyleabrahams <47188480+kyleabrahams@users.noreply.github.com>
2025-07-01 11:19:01 -0400

    Jul 1 1118 am for.

--- scripts/m3u_A-Z.py
+++ scripts/m3u_A-Z.py
@@ -2,8 +2,8 @@ import re
 from tqdm import tqdm
 
 # File paths
-input_file = '/Volumes/Kyle4tb1223/_Android/_M3U/___Servers/wanicelife Oct 14 2025_DAANM2945 SORTED.m3u'
-output_file = '/Volumes/Kyle4tb1223/_Android/_M3U/___Servers/wanicelife Oct 14 2025_DAANM2945 SORTED2.m3u'
+input_file = '/Volumes/Kyle4tb1223/_Android/_M3U/___fortv/1tv41.icu-Apr 21 2026_tv_channels_CANADA.m3u'
+output_file = '/Volumes/Kyle4tb1223/_Android/_M3U/___fortv/1tv41.icu-Apr 21 2026_tv_channels_CANADA_SORTED.m3u'
 
 # Group priorities
 group_priority = {"Canada": 1, "USA": 2, "UK": 3, "Other": 99}
