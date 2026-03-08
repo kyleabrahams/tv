e5c4e1034d3dafe012caace32d34995f8e96b502
kyleabrahams <47188480+kyleabrahams@users.noreply.github.com>
2026-03-08 12:08:24 -0400

    1208 p

--- scripts/build_epg_xml.py
+++ scripts/build_epg_xml.py
@@ -29,7 +29,7 @@ import pytz  # Timezone handling (needed for accurate EPG timestamps)
 
 from build_channels_list import CHANNELS # CHANNELS is your predefined channel list used to filter the EPG
   
-# build_epg_xml.py Mar 8 1027 a 
+# build_epg_xml.py Mar 8 1208 p 
 
 # python3 /Volumes/Kyle4tb1223/Documents/Github/tv/scripts/build_epg_xml.py
 
@@ -329,7 +329,7 @@ def build_fast_epg():
     os.makedirs(os.path.dirname(OUTPUT_XML), exist_ok=True)
 
     XMLTV_URLS = [
-        # "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS1.xml.gz", 
+        "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS1.xml.gz", 
         "https://epgshare01.online/epgshare01/epg_ripper_CA2.xml.gz",
         "https://epgshare01.online/epgshare01/epg_ripper_US2.xml.gz",
         "https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz"
