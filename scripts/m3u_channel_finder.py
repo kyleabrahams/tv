8814ce85ae40baf51a288bec60aaa373ee9e65d9
kyleabrahams <47188480+kyleabrahams@users.noreply.github.com>
2026-06-26 10:20:02 -0400

    1019

--- scripts/m3u_channel_finder.py
+++ scripts/m3u_channel_finder.py
@@ -24,7 +24,7 @@ M3U_FOLDERS = [
 OUTPUT_FOLDER = "/Volumes/Kyle4tb1223/_Android/_M3U/____Fetched/Channels"
 
 GROUP_KEYWORDS = [] # 1. Standalone search purely for the group-title tag
-KEYWORDS = ["Slice"] # 2. Keywords to search within the channel name / meta
+KEYWORDS = ["Alpha"] # 2. Keywords to search within the channel name / meta
 SERVER_KEYWORDS = []
 
 KEYWORDS_MAP = {
